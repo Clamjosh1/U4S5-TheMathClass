3c5657ed5b5328b8c4c174ff8f8574c5ed9d78fd
Clamjosh <Clamjosh1@users.noreply.github.com>
2024-12-16 16:20:19 -0500

    I finished rockpaperscissors

--- src/main/java/com/codedifferently/labs/partA/RockPaperScissor.java
+++ src/main/java/com/codedifferently/labs/partA/RockPaperScissor.java
@@ -1,9 +1,29 @@
 package com.codedifferently.labs.partA;
 
+import java.util.Random;
+
 public class RockPaperScissor {
 
     public static void main(String[] args) {
 
+        Random rand = new Random();
 
+        int numRounds = 10;
+
+        for (int i = 0; i < numRounds; i++) {
+            int choice  = rand.nextInt(3);
+
+            String result;
+            if (choice ==0) {
+                result="rock";
+            } else if (choice ==1) {
+                result="paper";
+            } else {
+                result = "scissors";
+            }
+
+
+            System.out.println("Round" + (i+1) + ": " + result);
+        }
     }
 }
