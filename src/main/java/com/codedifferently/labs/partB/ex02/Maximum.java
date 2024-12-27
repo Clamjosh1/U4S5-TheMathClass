c51e01775be20b556c35528f658b3d6e34cf4a37
Clamjosh <Clamjosh1@users.noreply.github.com>
2024-12-27 11:59:15 -0500

    just finished maximum

--- src/main/java/com/codedifferently/labs/partB/ex02/Maximum.java
+++ src/main/java/com/codedifferently/labs/partB/ex02/Maximum.java
@@ -1,4 +1,14 @@
 package com.codedifferently.labs.partB.ex02;
 
 public class Maximum {
+    public static void main(String[] args) {
+
+        int num1 = 12;
+        int num2 = 20;
+
+        int maximum = Math.max(num1, num2);
+
+        System.out.println("Maximum number of " + num1 + " and " + num2 + " is: " + maximum);
+
+    }
 }
