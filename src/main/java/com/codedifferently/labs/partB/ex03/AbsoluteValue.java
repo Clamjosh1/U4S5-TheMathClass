8e3d3dcc04ee2b16317d65c35b98b6fca71af286
Clamjosh <Clamjosh1@users.noreply.github.com>
2024-12-27 13:40:43 -0500

    finished

--- src/main/java/com/codedifferently/labs/partB/ex03/AbsoluteValue.java
+++ src/main/java/com/codedifferently/labs/partB/ex03/AbsoluteValue.java
@@ -3,5 +3,11 @@ package com.codedifferently.labs.partB.ex03;
 public class AbsoluteValue {
     public static void main(String[] args) {
         int a = - 445, b = 399, c = 33 - 212;
+
+        int sum = a + b + c;
+
+        int absoluteSum = Math.abs(sum);
+
+        System.out.println(absoluteSum);
     }
 }
